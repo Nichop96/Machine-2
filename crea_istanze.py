576fa949fbe8417ac3196514dfd113299c01c792
Nichop96 <48830373+Nichop96@users.noreply.github.com>
2019-12-04 16:58:12 +0100

    ciao

--- crea_istanze.py
+++ crea_istanze.py
@@ -3,37 +3,27 @@ import numpy as np
 
 
 def crea(plans):
-    actions = np.concatenate([p.actions for p in plans], axis=0)
-    db = []
-    for action in actions:
-        data = np.array([])
-        i = 0
-        for prec in action.oneHotprecondition:
-            data = np.concatenate([data, prec])
-            i += 1
-        for j in range(i, 3):
-            data = np.concatenate([data, np.zeros(450)])
-        i = 0
-        for pos in action.oneHotpositiveEffects:
-            data = np.concatenate([data, pos])
-            i += 1
-        for j in range(i, 1):
-            data = np.concatenate([data, np.zeros(450)])
-        i = 0
-        for neg in action.oneHotnegativeEffects:
-            data = np.concatenate([data, neg])
-            i+=1
-        for j in range(i, 1):
-            data = np.concatenate([data, np.zeros(450)])
-        d1 = action.oneHotAtction[:10]
-        d2 = action.oneHotAtction[10: 16]
-        d3 = action.oneHotAtction[16: 120]
-        d4 = action.oneHotAtction[120: 126]
-        d5 = action.oneHotAtction[126: 230]
-        d6 = action.oneHotAtction[230: 236]
-        d7 = action.oneHotAtction[236: 340]
-        d8 = action.oneHotAtction[340: 346]
-        d9 = action.oneHotAtction[346: 450]
-        db.append((data, [d1, d2, d3, d4, d5, d6, d7, d8, d9]))
 
+    num_train_actions = 3
+    db = []
+
+    for plan in plans:
+        actions = np.concatenate([plan.actions], axis=0)
+        for i in range(len(actions)):
+            data = np.array([])
+            if (i+num_train_actions) >= len(actions):
+                break
+
+            for j in range(num_train_actions):
+                data = np.concatenate([data, actions[i+j].oneHotAtction])
+            d1 = actions[i+num_train_actions].oneHotAtction[:10]
+            d2 = actions[i+num_train_actions].oneHotAtction[10: 16]
+            d3 = actions[i+num_train_actions].oneHotAtction[16: 120]
+            d4 = actions[i+num_train_actions].oneHotAtction[120: 126]
+            d5 = actions[i+num_train_actions].oneHotAtction[126: 230]
+            d6 = actions[i+num_train_actions].oneHotAtction[230: 236]
+            d7 = actions[i+num_train_actions].oneHotAtction[236: 340]
+            d8 = actions[i+num_train_actions].oneHotAtction[340: 346]
+            d9 = actions[i+num_train_actions].oneHotAtction[346: 450]
+            db.append((data, [d1, d2, d3, d4, d5, d6, d7, d8, d9]))
     return db
